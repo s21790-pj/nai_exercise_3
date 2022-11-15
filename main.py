95dc1924e214251d3e82fdbba05e5aab78a5ecef
donkamileq <61499782+donkamileq@users.noreply.github.com>
2022-11-15 19:48:35 +0100

    Add euclidean score and pearson score functions

--- main.py
+++ main.py
@@ -2,3 +2,96 @@ import argparse
 import json
 import numpy as np
 
+
+def build_arg_parser():
+    """
+
+    :return:
+    """
+    parser = argparse.ArgumentParser(description='Compute similarity score')
+    parser.add_argument('--user', dest='user', required=True, help='Name of user')
+    return parser
+
+
+def euclidean_score(dataset, user1, user2):
+    """
+
+    :param dataset:
+    :param user1:
+    :param user2:
+    :return:
+    """
+    if user1 not in dataset:
+        raise TypeError('Cannot find ' + user1 + ' in the dataset')
+
+    if user2 not in dataset:
+        raise TypeError('Cannot find ' + user2 + ' in the dataset')
+
+    # Movies rated by both user1 and user2
+    common_movies = {}
+
+    for item in dataset[user1]:
+        if item in dataset[user2]:
+            common_movies[item] = 1
+
+    # If there are no common movies between the users,
+    # then the score is 0
+    if len(common_movies) == 0:
+        return 0
+
+    squared_diff = []
+
+    for item in dataset[user1]:
+        if item in dataset[user2]:
+            squared_diff.append(np.square(dataset[user1][item] - dataset[user2][item]))
+
+    return 1 / (1 + np.sqrt(np.sum(squared_diff)))
+
+
+def pearson_score(dataset, user1, user2):
+    """
+
+    :param dataset:
+    :param user1:
+    :param user2:
+    :return:
+    """
+    if user1 not in dataset:
+        raise TypeError('Cannot find ' + user1 + ' in the dataset')
+
+    if user2 not in dataset:
+        raise TypeError('Cannot find ' + user2 + ' in the dataset')
+
+    # Movies rated by both user1 and user2
+    common_movies = {}
+
+    for item in dataset[user1]:
+        if item in dataset[user2]:
+            common_movies[item] = 1
+
+    num_ratings = len(common_movies)
+
+    # If there are no common movies between user1 and user2, then the score is 0
+    if num_ratings == 0:
+        return 0
+
+    # Calculate the sum of ratings of all the common movies
+    user1_sum = np.sum([dataset[user1][item] for item in common_movies])
+    user2_sum = np.sum([dataset[user2][item] for item in common_movies])
+
+    # Calculate the sum of squares of ratings of all the common movies
+    user1_squared_sum = np.sum([np.square(dataset[user1][item]) for item in common_movies])
+    user2_squared_sum = np.sum([np.square(dataset[user2][item]) for item in common_movies])
+
+    # Calculate the sum of products of the ratings of the common movies
+    sum_of_products = np.sum([dataset[user1][item] * dataset[user2][item] for item in common_movies])
+
+    # Calculate the Pearson correlation score
+    Sxy = sum_of_products - (user1_sum * user2_sum / num_ratings)
+    Sxx = user1_squared_sum - np.square(user1_sum) / num_ratings
+    Syy = user2_squared_sum - np.square(user2_sum) / num_ratings
+
+    if Sxx * Syy == 0:
+        return 0
+
+    return Sxy / np.sqrt(Sxx * Syy)
